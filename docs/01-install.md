9cb25cfbafaf819359e10b568ac814eb3e6302cb
Hein Bekker <hein@netbek.co.za>
2024-01-01 13:02:55 +0200

    Tweak wording

--- docs/01-install.md
+++ docs/01-install.md
@@ -73,7 +73,7 @@ Set `/path/to/dw-analytics` to the location of the repository on your machine.
 
 ## Uninstall
 
-To delete the Docker volumes and images, run:
+To delete all the data and Docker images, run:
 
 ```shell
 ./scripts/uninstall.sh
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -8,7 +8,7 @@ source "${scripts_dir}/variables.sh"
 
 cd "${root_dir}"
 
-read -p "Are you sure you want to delete the Docker volumes and images? (y/n): " answer
+read -p "Are you sure you want to delete all the data and Docker images? (y/n): " answer
 
 if [ "$answer" != "y" ]; then
     echo "${tput_red}Aborted.${tput_reset}"
