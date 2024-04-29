e9c7f2ba9985644201aa13263d999b3fbc445039
Hein Bekker <hein@netbek.co.za>
2024-04-29 05:29:21 +0200

    Handle datetime data type in ClickHouse 24.3

--- template_project/dbt/macros/dbt/incremental_predicates.sql
+++ template_project/dbt/macros/dbt/incremental_predicates.sql
@@ -2,15 +2,16 @@
     {% if is_incremental() %}
         (
             {% for source_watermark_column in source_watermark_columns %}
+                {% set column = target_watermark_column if target_watermark_column is not none else source_watermark_column %}
+                {% set data_type = model['columns'][column]['data_type'] %}
                 {% if not loop.first %}{{ operator }} {% endif %}
                 {{ source_watermark_column }} > (
-                    select toString(max(
-                        {%- if target_watermark_column is none -%}
-                            {{ source_watermark_column }}
-                        {%- else -%}
-                            {{ target_watermark_column }}
-                        {%- endif -%}
-                    )) from {{ this }}
+                    {# For ClickHouse >= v24.3 #}
+                    {%- if 'DateTime' in data_type -%}
+                        select toString(max({{ column }})) from {{ this }}
+                    {%- else -%}
+                        select max({{ column }}) from {{ this }}
+                    {%- endif -%}
                 )
             {% endfor %}
         )
