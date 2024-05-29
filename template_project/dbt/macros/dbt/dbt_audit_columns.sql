7edac758eeccfbe6dc38145adca6d427acba738e
Hein Bekker <hein@netbek.co.za>
2024-05-29 04:09:10 +0200

    Add relation alias parameter

--- template_project/dbt/macros/dbt/dbt_audit_columns.sql
+++ template_project/dbt/macros/dbt/dbt_audit_columns.sql
@@ -1,3 +1,5 @@
-{% macro dbt_audit_columns() %}
-    {{ var('dbt_audit_column_names') | join(', ') }}
-{% endmacro %}
+{% macro dbt_audit_columns(relation_alias=none) -%}
+    {% for column_name in var('dbt_audit_column_names') -%}
+        {% if relation_alias %}{{ relation_alias }}.{% endif %}{{ adapter.quote(column_name) }} as {{ column_name }}{% if not loop.last %},{% endif %}
+    {% endfor %}
+{%- endmacro %}
