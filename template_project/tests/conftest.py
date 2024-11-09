1d9b9f12bea36c1240765adb020a2f84a6e88cc8
Hein Bekker <hein@netbek.co.za>
2024-11-09 19:07:31 +0000

    Update imports

--- template_project/tests/conftest.py
+++ template_project/tests/conftest.py
@@ -1,13 +1,2 @@
-from package.database import create_connection_url
-from package.project import Project
+from package.tests.conftest import ch_settings, TestCHSettings  # noqa: F401
 from package.tests.fixtures.database import *  # noqa: F403
-from typing import Any, Generator
-
-import pytest
-
-project = Project.from_path(__file__)
-
-
-@pytest.fixture(scope="session")
-def ch_url() -> Generator[str, Any, None]:
-    yield create_connection_url(**project.test_ch_settings.model_dump())
