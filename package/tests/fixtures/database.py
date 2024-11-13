53207ab3e7e27d33ac65b5a09877a99841b40f8b
Hein Bekker <hein@netbek.co.za>
2024-11-13 18:45:29 +0000

    Remove unused commands

--- package/tests/fixtures/database.py
+++ package/tests/fixtures/database.py
@@ -1,9 +1,8 @@
 from package.config.settings import get_settings
 from package.database import CHAdapter, PGAdapter
-from sqlmodel import Session, SQLModel
-from typing import Any, Generator, List
+from sqlmodel import Session
+from typing import Any, Generator
 
-import pydash
 import pytest
 
 settings = get_settings()
@@ -12,42 +11,10 @@ settings = get_settings()
 class DBTest:
     @pytest.fixture(scope="session")
     def ch_adapter(self) -> Generator[CHAdapter, Any, None]:
-        yield CHAdapter(settings.test_ch)
-
-    @pytest.fixture(scope="session")
-    def ch_database(self, ch_adapter: CHAdapter) -> Generator[List[str], Any, None]:
-        # Get database names
-        databases = [table.schema for table in SQLModel.metadata.tables.values() if table.schema]
-        databases = pydash.uniq(databases)
-        databases = pydash.without(databases, "default")
-
-        with ch_adapter.create_client() as client:
-            for database in databases:
-                client.command(
-                    "DROP DATABASE IF EXISTS {database:Identifier};",
-                    parameters={"database": database},
-                )
-                client.command(
-                    "CREATE DATABASE {database:Identifier};",
-                    parameters={"database": database},
-                )
-
-        with ch_adapter.create_engine() as engine:
-            SQLModel.metadata.create_all(engine)
-
-        yield databases
-
-        with ch_adapter.create_client() as client:
-            for database in databases:
-                client.command(
-                    "DROP DATABASE IF EXISTS {database:Identifier};",
-                    parameters={"database": database},
-                )
+        yield CHAdapter(settings.test_clickhouse)
 
     @pytest.fixture(scope="function")
-    def ch_session(
-        self, ch_adapter: CHAdapter, ch_database: List[str]
-    ) -> Generator[Session, Any, None]:
+    def ch_session(self, ch_adapter: CHAdapter) -> Generator[Session, Any, None]:
         with ch_adapter.create_engine() as engine:
             session = Session(engine)
 
@@ -55,13 +22,6 @@ class DBTest:
 
         session.close()
 
-        with ch_adapter.create_client() as client:
-            for table in SQLModel.metadata.tables.values():
-                client.command(
-                    "TRUNCATE TABLE {schema:Identifier}.{name:Identifier};",
-                    parameters={"schema": table.schema, "name": table.name},
-                )
-
     @pytest.fixture(scope="session")
     def pg_adapter(self) -> Generator[PGAdapter, Any, None]:
-        yield PGAdapter(settings.test_pg)
+        yield PGAdapter(settings.test_postgres)
