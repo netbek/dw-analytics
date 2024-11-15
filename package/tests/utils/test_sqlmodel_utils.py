6a05ee0a0e6a03fd72a08872b7dbeeda070adef6
Hein Bekker <hein@netbek.co.za>
2024-11-15 08:50:27 +0000

    Add tests

--- package/tests/utils/test_sqlmodel_utils.py
+++ package/tests/utils/test_sqlmodel_utils.py
@@ -1,5 +1,10 @@
 from clickhouse_sqlalchemy import types
-from package.utils.sqlmodel_utils import get_pydantic_type, get_python_type, get_sqlalchemy_type
+from package.utils.sqlmodel_utils import (
+    get_pydantic_type,
+    get_python_type,
+    get_sqlalchemy_type,
+    parse_create_table_statement,
+)
 from sqlalchemy import Column
 
 import datetime
@@ -153,3 +158,210 @@ class TestGetSQLAlchemyType:
 
     def test_nullable_uuid(self, nullable_uuid_column):
         assert get_sqlalchemy_type(nullable_uuid_column) == "types.Nullable(types.UUID)"
+
+
+class TestParseCreateTableStatement:
+    def test_no_properties(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": None,
+            "version": None,
+            "is_deleted": None,
+            "primary_key": [],
+            "order_by": [],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_engine_version(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree(_peerdb_version)
+        PRIMARY KEY id
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": "_peerdb_version",
+            "is_deleted": None,
+            "primary_key": ["id"],
+            "order_by": [],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_engine_version_and_is_deleted(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree(_peerdb_version, _peerdb_is_deleted)
+        PRIMARY KEY id
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": "_peerdb_version",
+            "is_deleted": "_peerdb_is_deleted",
+            "primary_key": ["id"],
+            "order_by": [],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_primary_key_string(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree
+        PRIMARY KEY id
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": None,
+            "is_deleted": None,
+            "primary_key": ["id"],
+            "order_by": [],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_primary_key_tuple(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree
+        PRIMARY KEY (id, country)
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": None,
+            "is_deleted": None,
+            "primary_key": ["id", "country"],
+            "order_by": [],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_order_by_string(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree
+        ORDER BY id
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": None,
+            "is_deleted": None,
+            "primary_key": [],
+            "order_by": ["id"],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_order_by_tuple(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            country String,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree
+        ORDER BY (id, country)
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": None,
+            "is_deleted": None,
+            "primary_key": [],
+            "order_by": ["id", "country"],
+            "settings": {},
+        }
+
+        assert actual == expected
+
+    def test_settings(self):
+        statement = """
+        CREATE TABLE test_table
+        (
+            id UInt64,
+            updated_at DateTime default now(),
+            _peerdb_synced_at DateTime64(9) DEFAULT now64(),
+            _peerdb_is_deleted Int8,
+            _peerdb_version Int64
+        )
+        ENGINE = ReplacingMergeTree
+        SETTINGS allow_nullable_key = 1, index_granularity = 8192
+        """
+        actual = parse_create_table_statement(statement)
+        expected = {
+            "engine": "ReplacingMergeTree",
+            "version": None,
+            "is_deleted": None,
+            "primary_key": [],
+            "order_by": [],
+            "settings": {"allow_nullable_key": "1", "index_granularity": "8192"},
+        }
+
+        assert actual == expected
