11316943f75fcf141c0d46aa23251c5b2e72548b
Hein Bekker <hein@netbek.co.za>
2025-02-22 12:51:58 +0200

    Bump PeerDB

--- scripts/install.sh
+++ scripts/install.sh
@@ -61,7 +61,7 @@ install_tilt() {
 install_peerdb() {
     echo "${tput_yellow}Installing PeerDB ...${tput_reset}"
     uninstall_peerdb
-    git clone https://github.com/PeerDB-io/peerdb --branch v0.23.0
+    git clone https://github.com/PeerDB-io/peerdb --branch v0.25.1
     echo "${tput_green}Installed PeerDB${tput_reset}"
 }
 
